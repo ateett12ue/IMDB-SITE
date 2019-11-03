38f82e7ca3ea013ede611ba132f5ab0be712eeef
ateet <16ucs050@lnmiit.ac.in>
2019-11-03 15:36:06 +0530

    routing setup

---
+++ src/Pages/ListingPage.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class ListingPage extends Component {
+  render() {
+    return <div>Hello from listing page</div>;
+  }
+}
---
+++ src/Pages/WatchList.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class WatchList extends Component {
+  render() {
+    return <div>Hello from the watch list</div>;
+  }
+}
